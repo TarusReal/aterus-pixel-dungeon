82891633330ae69a641ab82fae611b6c2d22e617
AtrebasTheo <AtrebasTS@protonmail.com>
2025-08-17 12:51:44 +0200

    MudSnake rare alt Snake added

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
@@ -241,8 +241,7 @@ public class MobSpawner extends Actor {
 
 	//switches out regular mobs for their alt versions when appropriate
 	private static void swapMobAlts(ArrayList<Class<?extends Mob>> rotation) {
-		//float altChance = 1 / 50f * RatSkull.exoticChanceMultiplier();
-		float altChance = 1 / 2f * RatSkull.exoticChanceMultiplier();
+		float altChance = 1 / 45f * RatSkull.exoticChanceMultiplier();
 		for (int i = 0; i < rotation.size(); i++) {
 			if (Random.Float() < altChance) {
 				Class<? extends Mob> cl = rotation.get(i);
